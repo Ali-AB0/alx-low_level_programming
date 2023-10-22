bdb2427559afac57a4cc35611b834a0b37dac932
Ali-AB0 <aliaitbrahim2000@gmail.com>
2023-10-22 06:52:25 -0700

    update code

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
 * main - Determine if anumber is positive, negative or zero.
-(*0 is the number to be checked
+*0 is the number to be checked
 * Return: 0 on success
 */
+
 void positive_or_negative(int i)
 {
 if (i < 0)

f4f7642adbe57343844d0b1e2fc4c60aa7644b7b
Ali-AB0 <aliaitbrahim2000@gmail.com>
2023-10-30 17:18:53 -0700

    Task-0

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,7 @@
 #include "main.h"
-
+/* This is the main function. It prints a quote by Kevin Mitnick. */
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
 }

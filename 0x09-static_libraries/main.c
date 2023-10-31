8f6eee7d3d07f81d7030feb6cee0540e3a3f36fd
Ali-AB0 <aliaitbrahim2000@gmail.com>
2023-10-31 14:21:28 -0700

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,13 +1,13 @@
+#include <unistd.h>
 #include "main.h"
+
 /**
- * main - Entry point
- *
- * Description: 'It prints a quote by Kevin Mitnick'
- *
+ * main - Prints a quote
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n"
+"\t- Kevin Mitnick");
 return (0);
 }

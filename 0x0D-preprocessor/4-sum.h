02ab3a8d710f202d168567db342826019aa92282
Ali-AB0 <aliaitbrahim2000@gmail.com>
2023-11-06 12:43:00 -0800

    Task-4-sum

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+/**
+ * File: 4-sum.h
+ * @x: first int input
+ * @y: second int input
+ * Desc: function-like macro SUM(x, y)
+ * that computes the sum of the numbers x and y.
+ */
+#define SUM(x, y) ((x) + (y))
+#endif
